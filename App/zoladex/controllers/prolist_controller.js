b4fe91a39df74dfd1a134b4f0a209822b780a736
Dakuan <dom.barker808@gmail.com>
2011-11-23 20:26:11 +0000

    added pro list view and controller

---
+++ App/zoladex/controllers/prolist_controller.js
@@ -0,0 +1,12 @@
+﻿steal('jquery/controller',
+    'jquery/view/ejs',
+    'jquery/dom/form_params',
+    'jquery/controller/view'
+    )
+    .then(function ($) {
+        $.Controller('Zoladex.Controllers.ProList', {
+            init: function () {
+                alert('todo: load data from local db');
+            }
+        });
+    });
--- App/zoladex/zoladex.js
+++ App/zoladex/zoladex.js
@@ -5,6 +5,7 @@ steal(
     './fixtures/fixtures.js', // sets up fixtures for your models
     './controllers/intro_controller',
      './controllers/tabbar_controller',
+     './controllers/prolist_controller',
     function () { // configure your application
 
         steal.dev.log('jmvc init');
@@ -22,9 +23,11 @@ steal(
             $.mobile.touchOverflowEnabled = true;
         });
 
-        $(document).bind('pagebeforecreate', function (e, args) {
-
+        $(document).bind('pagebeforecreate', function (e) {
+            
             bindTabBar(e);
+
+            bindPageControllers(e);
         });
     }
 
@@ -36,19 +39,26 @@ steal(
             folder = 'hcp';
         }
 
-        if ($(e.target).filter('#.calendarPage').length > 0) {
+        if ($(e.target).filter('.calendarPage').length > 0) {
             folder = 'calendar';
         }
 
-        if ($(e.target).filter('#.progressPage').length > 0) {
+        if ($(e.target).filter('.progressPage').length > 0) {
             folder = 'progress';
         }
 
-        if ($(e.target).filter('#.supportPage').length > 0) {
+        if ($(e.target).filter('.supportPage').length > 0) {
             folder = 'support';
         }
 
         $('.tabBarContainer').zoladex_tab_bar('destroy');
 
         $('.tabBarContainer').zoladex_tab_bar({ folder: folder });
+    }
+
+    function bindPageControllers(e) {
+
+        if ($(e.target).filter('#ProfessionalListPage').length > 0) {
+            $('#ProfessionalListPage').zoladex_pro_list();
+        }    
     }
