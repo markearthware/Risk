9dac41399ea11d5bcf80727b03941950558c078a
Mark Short <mark.ross.short@gmail.com>
2011-11-24 10:59:00 +0000

    web sql added

--- App/zoladex/controllers/prolist_controller.js
+++ App/zoladex/controllers/prolist_controller.js
@@ -1,12 +1,21 @@
 ﻿steal('jquery/controller',
     'jquery/view/ejs',
     'jquery/dom/form_params',
-    'jquery/controller/view'
+    'jquery/controller/view',
+    'lib/WebSQL/db.js'
     )
     .then(function ($) {
         $.Controller('Zoladex.Controllers.ProList', {
             init: function () {
-                alert('todo: load data from local db');
+                localStorageDB.init(function () {
+                    
+                    //find the list
+                    for (var i = 0; i < localStorageDB.hcps.length; i++) {
+                        alert(localStorageDB.hcps[i].Title);
+                        //add an element to the list
+                        
+                    }
+                });
             }
         });
     });
---
+++ App/zoladex/lib/WebSQL/db.js
@@ -0,0 +1,64 @@
+﻿var localStorageDB = (function () {
+
+    var db = null,
+    latest = 0,
+    hcps = [],
+    loadedCallback = null;
+
+    function initDb(callback) {
+        loadedCallback = callback;
+        try {
+            if (window.openDatabase) {
+                db = openDatabase("zoladexDB", "1.0", "HTML 5 Database API example", 200000);
+                if (db) {
+                    db.transaction(function (tx) {
+                        tx.executeSql('CREATE TABLE IF NOT EXISTS HealthcareProfessionals (Id unique, Title, FirstName, Surname, PracticeName, Telephone, Street, Town)', [], function (tx, result) {
+                            var date = new Date();
+                            var ticks = date.getTime();
+                            var someText = "someText";
+                            tx.executeSql('INSERT INTO HealthcareProfessionals (Id, Title, FirstName, Surname, PracticeName, Telephone, Street, Town) VALUES (?,?,?,?,?,?,?,?)', [ticks, someText, someText, someText, someText, someText, someText, someText]);
+                            load();
+                        });
+                    });
+                }
+            } else {
+                alert('Web Databases not supported');
+            }
+        } catch (e) {
+            alert('error occurred during DB init, Web Database supported?');
+        }
+    }
+
+    function load() {
+        db.transaction(function (tx) {
+            tx.executeSql('SELECT * FROM HealthcareProfessionals', [], function (tx, results) {
+                if (results.rows && results.rows.length) {
+                    for (var i = 0; i < results.rows.length; i++) {
+                        hcps.push(results.rows.item(i));
+                    }
+                    // fire loaded callback if not null
+                    if (loadedCallback) loadedCallback();
+                }
+            }, function (tx) {
+                alert('load error occurred, please reset DB');
+            });
+        });
+    }
+
+    //    function addCar(manufacturer) {
+    //        var date = new Date();
+    //        var ticks = date.getTime();
+    //        db.transaction(function (tx) {
+    //            tx.executeSql('INSERT INTO cars (id, manu) VALUES (?, ?)', [ticks, manufacturer]);
+    //            clear();
+    //            load();
+    //        });
+    //    }
+
+    return {
+        init: initDb,
+        hcps: hcps
+    };
+})();
+
+
