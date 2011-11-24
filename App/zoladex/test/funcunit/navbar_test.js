5eaab73d46ab0f06e106bb05cab344fd749d6977
Dakuan <dom.barker808@gmail.com>
2011-11-24 13:41:57 +0000

    added nav bar funcunit test

---
+++ App/zoladex/test/funcunit/navbar_test.js
@@ -0,0 +1,11 @@
+﻿steal("funcunit", function () {
+    module("zoladex test", {
+        setup: function () {
+            S.open("//zoladex/zoladex.html");
+        }
+    });
+
+    test("Copy Test", function () {
+        ok(S("h1").first().text('Header'), "header text correct");
+    });
+})
