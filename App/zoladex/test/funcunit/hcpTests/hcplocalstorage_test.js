569087519962b428026b36092dbe1b8185e9b187
Mark Short <mark.ross.short@gmail.com>
2011-11-29 15:38:06 +0000

    updated some tests

---
+++ App/zoladex/test/funcunit/hcpTests/hcplocalstorage_test.js
@@ -0,0 +1,23 @@
+﻿steal("funcunit", function () {
+    module("Healthcare Professionals Local Storage loads data", {
+        setup: function () {
+
+            S.open("../index.html", function() {
+                S('#btnContinue').exists().click();
+
+                S('.hcpTab').exists().click();
+
+                S('#Hcps').exists().click();
+
+                S('#HcpListPage').exists();
+            });
+        }
+    });
+
+    test("Check dummy record from DB is Dr Mark Short", function () {
+        ok(S('#HcpListList li').size(function (size) {
+            return size > 0;
+        }), "at least one record in list");
+        ok(S('#HcpListList a').first().text("Dr Mark Short"), "Dummy record from DB is Dr Mark Short");
+    });
+})
