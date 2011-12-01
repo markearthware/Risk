04524b61b667910a6aca247a994003ba4e02cd57
Dakuan <dom.barker808@gmail.com>
2011-12-01 09:41:46 +0000

    added symptom list page

--- App/zoladex/test/funcunit/funcunit.js
+++ App/zoladex/test/funcunit/funcunit.js
@@ -4,10 +4,14 @@ steal("funcunit")
  "./navBarTests/calendarbutton_test.js",
  "./navBarTests/progressbutton_test.js",
  "./navBarTests/supportbutton_test.js",
+    //hcp tests
  "./hcpTests/hcplistbutton_test.js",
  "./hcpTests/hcplocalstorage_test.js",
  "./hcpTests/hcpdetailspage_test.js",
  "./hcpTests/hcpadd_test.js",
  "./hcpTests/hcpedit_test.js", 
- "./hcpTests/hcpdelete_test.js"
+ "./hcpTests/hcpdelete_test.js",
+    
+    //symptom tests
+    "./progressTests/symptomTests/symptomlist_test.js"
 );
---
+++ App/zoladex/test/funcunit/progressTests/symptomTests/symptomlist_test.js
@@ -0,0 +1,22 @@
+﻿steal("funcunit", function () {
+    module("Symptom list page", {
+        setup: function () {
+
+            S.open("../index.html", function () {
+
+                S('#btnContinue').exists().click();
+
+                S('.progressTab').exists().click();
+
+                S('#SymptomsList').exists().click();
+
+                S('#SymptomsListPage').exists();
+            });
+        }
+    });
+
+    test("Symptom list header header", function () {
+
+        equal(S('#SymptomsListPage h1').text(), "Symptom History", "header is Symptom History");
+    });
+})
