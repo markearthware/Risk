4910b564a197260f55ae008179cdf6c8d21dcbaa
Mark Short <mark.ross.short@gmail.com>
2011-12-02 14:58:43 +0000

    push before pull

--- App/zoladex/models/hcp.js
+++ App/zoladex/models/hcp.js
@@ -1,6 +1,6 @@
 steal('jquery/model', function () {
 
-    $.Model('Zoladex.Models.Hcp',
+  Zoladex.Models.WebSqlModel('Zoladex.Models.Hcp',
   {
       findAll: function (params) {
           if (params && params.basicdetails) {
@@ -11,24 +11,13 @@ steal('jquery/model', function () {
 
       findOne: function (id) {
 
-          var result = localStorageDB.getSingleRow('SELECT * FROM HealthcareProfessionals WHERE Id =' + id, this);
+          var result = localStorageDB.getSingleRow('SELECT * FROM HealthcareProfessionals WHERE id =' + id, this);
           steal.dev.log("result is:");
           steal.dev.log(result);
           return result;
       },
-
-      create: function (newhcp, success, error) {
-          return localStorageDB.addHcp(newhcp, success, error);
-      },
-
-      update: function (id, hcp, success, error) {
-          return localStorageDB.updateHcp(hcp, success, error);
-      },
-
-      destroy: function (id, success, error) {
-          return localStorageDB.deleteHcp(id, success, error);
-      }
-
+      
+       tableName: "HealthcareProfessionals"
   },
   { 
       FullName: function () {
