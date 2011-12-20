ea91820df0b36bfcb7caad4814d51daa3bd42cd1
Mark Short <mark.ross.short@gmail.com>
2011-12-20 15:08:10 +0000

    fixed some view encoding issues

--- App/zoladex/models/patientsymptomlistitem.js
+++ App/zoladex/models/patientsymptomlistitem.js
@@ -3,7 +3,7 @@ steal('jquery/model', function () {
     Zoladex.Models.WebSqlModel('Zoladex.Models.PatientSymptomListItem',
   {
     findAll: function () {
-        return localStorageDB.getRows('SELECT ps.id, ps.DateTime, ps.SymptomId, s.Description, s.WarningSign FROM PatientSymptoms as ps INNER JOIN Symptoms as s on ps.SymptomId=s.id ORDER BY ps.DateTime', this);
+        return localStorageDB.getRows('SELECT ps.id, ps.DateTime, ps.SymptomId, s.Description, s.WarningSign FROM PatientSymptoms as ps INNER JOIN Symptoms as s on ps.SymptomId=s.id ORDER BY ps.DateTime DESC', this);
     },
     attributes: {
         DateTime: 'date'
