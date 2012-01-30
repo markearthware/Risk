3f224a391f29dc2b532c3f5593be86207a1da56d
Dakuan <dom.barker808@gmail.com>
2012-01-30 13:11:16 +0000

    bug fixes, dialog refactor

--- App/zoladex/controllers/usefulorgs_controller.js
+++ App/zoladex/controllers/usefulorgs_controller.js
@@ -4,18 +4,32 @@ steal('jquery/controller',
     'jquery/controller/view')
     .then(function ($) {
         $.Controller('Zoladex.Controllers.UsefulOrgs', {
-    },
+        },
     {
         init: function () {
             if (navigator.network) {
                 var networkState = navigator.network.connection.type;
-                
+
                 if (networkState == Connection.NONE || networkState == Connection.UNKNOWN) {
                     //Clear href attr and remove link icon
                     $('#weblink').attr('href', 'dialog/notconnected.htm');
                     $('#weblink').parent().attr('data-icon', 'false');
                 }
             }
+        },
+        '#weblink click': function (el, ev) {
+
+            ev.preventDefault();
+
+            var dialog = $.View('../../../views/alerts/leaveAppDialog', {
+                
+                toLink: el.attr('href'), 
+                fromLink: document.location.href
+            });
+
+            this.element.parent().append(dialog);
+
+            $.mobile.changePage($('#leaveAppDialog'));
         }
     });
-});
+    });
