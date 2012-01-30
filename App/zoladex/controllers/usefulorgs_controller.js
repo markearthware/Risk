85b10ee88826667178385c16bf1aa23a5b1d9afd
Dakuan <dom.barker808@gmail.com>
2012-01-30 16:07:39 +0000

    widget initialised before page transition

--- App/zoladex/controllers/usefulorgs_controller.js
+++ App/zoladex/controllers/usefulorgs_controller.js
@@ -5,35 +5,40 @@ steal('jquery/controller',
     .then(function ($) {
         $.Controller('Zoladex.Controllers.UsefulOrgs', {
         },
-    {
-        init: function () {
-            if (navigator.network) {
-                var networkState = navigator.network.connection.type;
+        {
+            init: function () {
+                if (navigator.network) {
+                    var networkState = navigator.network.connection.type;
 
-                if (networkState == Connection.NONE || networkState == Connection.UNKNOWN) {
-                    //Clear href attr and remove link icon
-                    $('#weblink').attr('href', 'dialog/notconnected.htm');
-                    $('#weblink').parent().attr('data-icon', 'false');
+                    if (networkState == Connection.NONE || networkState == Connection.UNKNOWN) {
+                        //Clear href attr and remove link icon
+                        $('#weblink').attr('href', 'dialog/notconnected.htm');
+                        $('#weblink').parent().attr('data-icon', 'false');
+                    }
                 }
+            },
+
+            '.weblink click': function (el, ev) {
+
+                alert('clicky');
+                
+                ev.preventDefault();
+
+                if ($('#leaveAppDialog')) {
+                    $('#leaveAppDialog').remove();
+                }
+
+                var dialog = $.View('../../../views/alerts/leaveAppDialog', {
+
+                    toLink: el.attr('href'),
+                    fromLink: document.location.href
+                });
+
+                this.element.parent().append(dialog);
+
+                $('#leaveAppDialog').page();
+
+                $.mobile.changePage($('#leaveAppDialog'));
             }
-        },
-        '.weblink click': function (el, ev) {
-
-            ev.preventDefault();
-
-            if ($('#leaveAppDialog')) {
-                $('#leaveAppDialog').remove();
-            }
-
-            var dialog = $.View('../../../views/alerts/leaveAppDialog', {
-
-                toLink: el.attr('href'),
-                fromLink: document.location.href
-            });
-
-            this.element.parent().append(dialog);
-
-            $.mobile.changePage($('#leaveAppDialog'));
-        }
-    });
+        });
     });
